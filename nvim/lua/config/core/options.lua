0e479aa42bb9fa8273ba1994ed463cddfe182d3f
Junior RT <juniorro98@outlook.com>
2023-01-30 19:16:57 -0500

    add scrolloff option

--- nvim/lua/config/core/options.lua
+++ nvim/lua/config/core/options.lua
@@ -3,6 +3,7 @@ local opt = vim.opt -- for conciseness
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.scrolloff = 3 -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
