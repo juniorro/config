2b6c52ac4c82b1e96fb553c99cf0287d062508e6
Junior RT <juniorro98@outlook.com>
2024-02-02 17:15:38 -0500

    added two line for video editing

--- shellscript/script.sh
+++ shellscript/script.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+for i in *.mkv; do ffmpeg -i "$i" -codec copy "${i%.*}.mp4" done;
+
+
+for file in /home/junior/mp4/*.mp4; do jumpcutter -i "$file" -o "/home/junior/mp4/output/${file}"; done
