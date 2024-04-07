fb306b62ae3d9aa0d7deff236b3e2766ae350414
Junior RT <juniorro98@outlook.com>
2024-04-07 00:31:03 -0400

    Update cutter.sh

--- shellscript/cutter.sh
+++ shellscript/cutter.sh
@@ -1,6 +1,6 @@
 for file in /home/junior/mp4/*.mp4; do jumpcutter -i "$file" -o "/home/junior/mp4/output/${file}"; done
 for file in *.mp4; do jumpcutter -i "$file" -o "./output/${file}"; done
-for file in *.mp4; do ffmpeg -i "$file" -o "./output/${file}"; done
+for file in *.mp4; do ffmpeg -i "$file" "./output/${file}"; done
 for file in *.mp4; do ffmpeg -i "$file" "./audio/${file%.*}.mp3"; done
 for file in *.mp4; do ffmpeg -i "$file" -i "./audio/${file%.*}.mp3" -map 0:v -map 1:a -c:v copy -shortest "./withaudio/${file}"; done
 ffmpeg -safe 0 -f concat -i videos.txt -c copy onevideo.mp4
